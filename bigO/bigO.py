0744f22db8d0e4920871bc8c13760eb0b527462a
Haruto Mori <baystarsman@outlook.jp>
2023-01-16 18:00:32 +0100

    add an example of O(n^2)

--- bigO/bigO.py
+++ bigO/bigO.py
@@ -17,9 +17,23 @@ def print_nums(n):
         print(j, end=" ")
 
 print_nums(10)
+print()
 
 """
 in this case time complexity: O(n+n) = O(2n)
 however, we can simplifiy it to remove the constant.
 thus, time complexity: O(n) as well as function above.
+"""
+
+
+# what if there's a nested loop?
+def nest_print_num(n):
+    for i in range(n):
+        for j in range(n):
+            print(i, j)
+
+nest_print_num(10)
+
+"""
+in this case tmie complexity: O(n*n) = O(n^2)
 """
