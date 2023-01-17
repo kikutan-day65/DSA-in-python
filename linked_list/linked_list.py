0fdfb288ce8914d7bff5502e342c841f37c550d4
Haruto Mori <baystarsman@outlook.jp>
2023-01-17 17:44:05 +0100

    explain what a node is and what linked list looks like

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -8,3 +8,52 @@ time complexity:
     - remove the elemnt in the middle: O(n)
     - look up the element: O(n)
 """
+
+"""
+What is nodes?
+    - value & pointer [4]->
+
+    - node is like a nested dictionary:
+        node = {
+                "value": 4,
+                "next": pointer
+               }
+    
+    - exmaple:
+
+        [11]->[3]->[23]->[4]->None
+
+    head = {
+            "value": 11,
+            "next": {
+                "value": 3,
+                "next": {
+                    "value": 23,
+                    "next": {
+                        "value": 4,
+    tail -------------> "next": None
+                    }
+                }
+            }
+        }
+"""
+
+head = {
+            "value": 11,
+            "next": {
+                "value": 3,
+                "next": {
+                    "value": 23,
+                    "next": {
+                        "value": 4,
+                        "next": None
+                    }
+                }
+            }
+        }
+
+print(head["value"])
+print(head["next"]["value"])
+print(head["next"]["next"]["value"])
+print(head["next"]["next"]["next"]["value"])
+print()
