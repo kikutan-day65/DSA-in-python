c4222481681fc883c4987fca59e5455b1346afad
Haruto Mori <baystarsman@outlook.jp>
2023-01-17 18:12:13 +0100

    add new methos in the class called print_list

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -7,7 +7,7 @@ class Node:
 class LinkedList:
     def __init__(self, value):
         # createing a new node
-        new_node = Node(value) 
+        new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
@@ -21,11 +21,18 @@ class LinkedList:
         # createing a new node
         # add node to fornt
         pass
-    
+
     def insert(self, index, value):
         # createing a new node
         # insert node to the given index
         pass
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
 
 my_linked_list = LinkedList(4)
 print(my_linked_list.head.value)
