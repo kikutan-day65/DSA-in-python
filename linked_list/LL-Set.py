1abeeb9c1eb43dc97e06181de709e287b266bb28
Haruto Mori <baystarsman@outlook.jp>
2023-01-17 20:54:08 +0100

    solve the problem with lecture video

--- linked_list/LL-Set.py
+++ linked_list/LL-Set.py
@@ -74,12 +74,12 @@ class LinkedList:
             temp = temp.next
         return temp
         
-    ## WRITE SET_VALUE METHOD HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
+    def set_value(self, index,value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
     
 
 
