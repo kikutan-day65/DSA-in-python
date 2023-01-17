ff85464481285bee84e06befe45f382ed90b0f31
Haruto Mori <baystarsman@outlook.jp>
2023-01-17 17:26:43 +0100

    add time complexity for the linked list

---
+++ linked_list/linked_list.py
@@ -0,0 +1,10 @@
+"""
+time complexity:             
+    - appending the elemnt: O(1)
+    - remove elemnt at the end: O(n) cuz needed to iterate through the linked list
+    - add at the front: O(1)
+    - remove element at the front: O(1)
+    - insert the element: O(n)
+    - remove the elemnt in the middle: O(n)
+    - look up the element: O(n)
+"""
