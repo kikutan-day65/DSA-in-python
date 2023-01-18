66ba03e37e415e2910572995b6a52346716330a7
Haruto Mori <baystarsman@outlook.jp>
2023-01-18 11:01:15 +0100

    solve the doubly linked list appending with a lecture video

--- doubly_linked_list/Append.py
+++ doubly_linked_list/Append.py
@@ -18,13 +18,17 @@ class DoublyLinkedList:
             print(temp.value)
             temp = temp.next
         
-    ## WRITE APPEND METHOD HERE ##
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
-  
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
 
   
 
