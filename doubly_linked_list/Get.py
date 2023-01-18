009b69a673d6110adf3a1200d2d96c913b53a2bc
Haruto Mori <baystarsman@outlook.jp>
2023-01-18 13:16:58 +0100

    solve get an element in the doubly linked list with lecture video

--- doubly_linked_list/Get.py
+++ doubly_linked_list/Get.py
@@ -71,12 +71,21 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
 
-    ### WRITE GET METHOD HERE ###
-    #                           #
-    #                           #
-    #                           #
-    #                           #
-    #############################
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        
+        temp = self.head
+
+        # check from which side the element that i wanna get is close
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
 
 
 
