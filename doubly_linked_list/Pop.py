aaf1c43de4cf6aeff7d4fd9d5d1086c0ef746c76
Haruto Mori <baystarsman@outlook.jp>
2023-01-18 11:18:59 +0100

    slove problem of pop in the doubly linked list with lecture video

--- doubly_linked_list/Pop.py
+++ doubly_linked_list/Pop.py
@@ -30,12 +30,21 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
-    ### WRITE POP METHOD HERE ###
-    #                           #
-    #                           #
-    #                           #
-    #                           #
-    #############################
+    def pop(self):
+        if self.length == 0:
+            return None
+        
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.legth -= 1
+
+        return temp          
   
 
   
