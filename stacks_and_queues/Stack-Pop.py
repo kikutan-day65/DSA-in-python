f0d754d2d317da288ade3f6e712768e25be63702
Haruto Mori <baystarsman@outlook.jp>
2023-01-19 10:56:44 +0100

    solve a problem of pop from the stack but it dosn't work

--- stacks_and_queues/Stack-Pop.py
+++ stacks_and_queues/Stack-Pop.py
@@ -26,12 +26,13 @@ class Stack:
         self.height += 1
         return True
 
-    ### WRITE POP METHOD HERE ###
-    #                           #
-    #                           #
-    #                           #
-    #                           #
-    #############################
+    def pop(self):
+        if self.height == 0:
+            return None
+        else:
+            temp = self.top
+            self.top = self.top.next
+            temp.next = None
     
 
     
