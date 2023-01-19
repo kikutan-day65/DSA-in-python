051217e7cf8ca7dddd081899fd7488b031241424
Haruto Mori <baystarsman@outlook.jp>
2023-01-19 11:20:07 +0100

    fix my code a bit, my answer was almost correct

--- stacks_and_queues/Queue-Enqueue.py
+++ stacks_and_queues/Queue-Enqueue.py
@@ -19,12 +19,13 @@ class Queue:
         
     def enqueue(self, value):
         new_node = Node(value)
-        if self.length == 0:
+        if self.first is None:
             self.first = new_node
+            self.last = new_node
         else:
             self.last.next = new_node
             self.last = new_node
-            self.length += 1
+        self.length += 1
         
 
 
