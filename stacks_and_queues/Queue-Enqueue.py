d8e94d70f7c187ca8a31002d72900f7ba837cfd5
Haruto Mori <baystarsman@outlook.jp>
2023-01-19 11:17:16 +0100

    solve th problem of enqueue, and it seems working

--- stacks_and_queues/Queue-Enqueue.py
+++ stacks_and_queues/Queue-Enqueue.py
@@ -17,12 +17,14 @@ class Queue:
             print(temp.value)
             temp = temp.next
         
-    ## WRITE ENQUEUE METHOD HERE ##
-    #                             #
-    #                             #
-    #                             #
-    #                             #
-    ###############################
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+            self.length += 1
         
 
 
