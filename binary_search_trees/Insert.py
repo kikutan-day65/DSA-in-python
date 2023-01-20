516fabb4e4a940e6693dbf6137feafdfacb0074b
Haruto Mori <baystarsman@outlook.jp>
2023-01-20 13:16:23 +0100

    solve the insertion to the BST with a lecture video

--- binary_search_trees/Insert.py
+++ binary_search_trees/Insert.py
@@ -3,20 +3,40 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
-        
+
 
 class BinarySearchTree:
     def __init__(self):
         self.root = None
 
-    ## WRITE INSERT METHOD HERE ##
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
+    def insert(self, value):
+        # 1. create a new node
+        # ※ if new node == temp, return False
+        # 2. if <, go to left if >, go to right
+        # 3. if none, insert new node, else, move to next
+        # 4. iterate
+        # ※use temp variable to follew a node
+        new_node = Node(value)
 
+        if self.root is None:
+            self.root = new_node
+            return True
 
+        temp = self.root
+
+        while True:
+            if new_node.value == temp.value:
+                return False
+
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
 
 
 my_tree = BinarySearchTree()
@@ -32,10 +52,9 @@ my_tree.insert(3)
 """
 
 
-print('Root:', my_tree.root.value)            
-print('Root->Left:', my_tree.root.left.value)        
-print('Root->Right:', my_tree.root.right.value)        
-
+print('Root:', my_tree.root.value)
+print('Root->Left:', my_tree.root.left.value)
+print('Root->Right:', my_tree.root.right.value)
 
 
 """
