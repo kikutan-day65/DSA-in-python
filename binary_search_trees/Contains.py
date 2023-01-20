04057c464ca4f2da867d249b3dcb7b2912deeb71
Haruto Mori <baystarsman@outlook.jp>
2023-01-20 13:38:33 +0100

    solve a problem with a video lecture

--- binary_search_trees/Contains.py
+++ binary_search_trees/Contains.py
@@ -29,13 +29,16 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
 
-    ## WRITE CONTAINS METHOD HERE ##
-    #                              #
-    #                              #
-    #                              #
-    #                              #
-    ################################
-        
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
 
 
 
