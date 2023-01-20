7624ff4ca35e0059ae2b658b35422bcc7919ae46
Haruto Mori <baystarsman@outlook.jp>
2023-01-20 12:51:44 +0100

    add constructor for binary search tree

--- binary_search_trees/Constructor.py
+++ binary_search_trees/Constructor.py
@@ -1,20 +1,13 @@
-# class Node:
-    ## WRITE NODE CONSTRUCTOR HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
         
 
-# class BinarySearchTree:
-    ## WRITE BST CONSTRUCTOR HERE ##
-    #                              #
-    #                              #
-    #                              #
-    #                              #
-    ################################
-
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
 
 
 
