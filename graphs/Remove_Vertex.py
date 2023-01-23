db8ddfe861e589104090c7e2172a54c901d78fdc
Haruto Mori <baystarsman@outlook.jp>
2023-01-23 12:34:05 +0100

    build a function to remove vertex

--- graphs/Remove_Vertex.py
+++ graphs/Remove_Vertex.py
@@ -29,14 +29,13 @@ class Graph:
             return True
         return False
 
-    ## WRITE REMOVE_VERTEX METHOD HERE ##
-    #                                   #
-    #                                   #
-    #                                   #
-    #                                   #
-    ##################################### 
-
-
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
 
 
 my_graph = Graph()
