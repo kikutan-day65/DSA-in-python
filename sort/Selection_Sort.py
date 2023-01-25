aeb45333afa8b26bf7025e35266541ad6ced8513
Haruto Mori <baystarsman@outlook.jp>
2023-01-25 19:58:49 +0100

    create a slection sort function

--- sort/Selection_Sort.py
+++ sort/Selection_Sort.py
@@ -1,10 +1,14 @@
-## WRITE SELECTION_SORT FUNCTION HERE ##
-#                                      #
-#                                      #
-#                                      #
-#                                      #
-######################################## 
-    
+def selection_sort(my_list):
+    for i in range(len(my_list) - 1):
+        min_idx = i
+        for j in range(i + 1, len(my_list)):
+            if my_list[j] < my_list[min_idx]:
+                min_idx = j
+        if i != min_idx:
+            temp = my_list[i]
+            my_list[i] = my_list[min_idx]
+            my_list[min_idx] = temp
+    return my_list
 
 
 
