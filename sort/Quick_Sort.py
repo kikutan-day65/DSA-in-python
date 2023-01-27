893546c554e3152b7977f7bfc48c32f328223ca1
Haruto Mori <baystarsman@outlook.jp>
2023-01-27 10:05:48 +0100

    create a quick sort functino

--- sort/Quick_Sort.py
+++ sort/Quick_Sort.py
@@ -15,13 +15,12 @@ def pivot(my_list, pivot_index, end_index):
     return swap_index
 
 
-## WRITE QUICK_SORT_HELPER FUNCTION HERE ##
-#                                         #
-#                                         #
-#                                         #
-#                                         #
-###########################################
-    
+def quick_sort_helper(my_list, left, right):
+    if left < right:
+        pivot_index = pivot(my_list, left, right)
+        quick_sort_helper(my_list, left, pivot_index - 1)
+        quick_sort_helper(my_list, pivot_index + 1, right)
+    return my_list
 
 def quick_sort(my_list):
     quick_sort_helper(my_list, 0, len(my_list)-1)
